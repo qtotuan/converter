f7772d9156a0eae19268ada924bed3150ebabf4b
qtotuan <quynh.totuan@gmail.com>
2017-10-17 12:45:39 +0100

    Add hundreds and thousands

--- lib/num_word_converter.rb
+++ lib/num_word_converter.rb
@@ -19,37 +19,62 @@ module Converter
       @@tens[val] ? @@tens[val] : ""
     end
 
-    def in_english
-      string = ""
+    def get_six_digits(value)
+      result = ""
+      remainder = value % 1000
+      thousands = (value - remainder) / 1000
+      result += "#{main(thousands)} thousand"
+      result += " #{main(remainder)}" unless remainder == 0
+      return result
+    end
+
+    def get_three_digits(value)
+      result = ""
+      remainder = value % 100
+      hundreds = (value - remainder) / 100
+      result += "#{main(hundreds)} hundred"
+      result += " #{main(remainder)}" unless remainder == 0
+      return result
+    end
+
+    def get_two_digits(value)
+      return @@specials[value] if @@specials[value]
+
+      result = ""
+      remainder = value % 10
+      tens = (value - remainder) / 10
+      result += @@tens[tens]
+      result += "-#{@@specials[remainder]}" unless remainder == 0
+      return result
+    end
+
+    def main(value)
+      sign = ""
 
       # remove decimals
-      @value > 0 ? @value = @value.floor : @value = @value.ceil
+      value > 0 ? value = value.floor : value = value.ceil
 
       # add "negative" prefix for anything < 0
-      if @value < 0
-        string = "negative "
-        @value = @value.abs
+      if value < 0
+        sign = "negative "
+        value = value.abs
       end
 
       # return numbers from 1-19 with simple lookup
-      if @@specials[@value]
-        return string + @@specials[@value]
+      return sign + @@specials[value] if @@specials[value]
+
+      if value >= 1000
+        return sign + get_six_digits(value)
+      elsif value >= 100
+        return sign + get_three_digits(value)
+      else
+        return sign + get_two_digits(value)
       end
-
-      # return nil for numbers > 99
-      if @value/10 >= 10
-        return nil
-      end
-
-      # for full tens, like 30
-      string = string + get_tens((@value/10).floor)
-
-      # for tens with remainder, like 32
-      if @value % 10 > 0
-        string = string + "-" + get_specials((@value % 10).floor)
-      end
-
-      return string
     end
+
+    def in_english
+      main(@value)
+    end
+
   end
 end
--- spec/num_word_converter_spec.rb
+++ spec/num_word_converter_spec.rb
@@ -2,39 +2,76 @@ require "spec_helper"
 
 RSpec.describe "Converter" do
   describe ".in_english" do
-    it 'converts a positive integer into English words' do
+    it 'converts 0 to "zero"' do
+      expect(Converter::EnglishNumber.new(0).in_english).to eq("zero")
+      expect(Converter::EnglishNumber.new(0.5).in_english).to eq("zero")
+      expect(Converter::EnglishNumber.new(-0.5).in_english).to eq("zero")
+    end
+
+    it 'converts a one- and two-digit positive integer into English words' do
       expect(Converter::EnglishNumber.new(6).in_english).to eq("six")
       expect(Converter::EnglishNumber.new(11).in_english).to eq("eleven")
       expect(Converter::EnglishNumber.new(22).in_english).to eq("twenty-two")
       expect(Converter::EnglishNumber.new(30).in_english).to eq("thirty")
     end
 
-    it 'converts a positive decimal into English words' do
+    it 'converts a one- and two-digit positive decimal into English words' do
       expect(Converter::EnglishNumber.new(6.5).in_english).to eq("six")
       expect(Converter::EnglishNumber.new(24.5).in_english).to eq("twenty-four")
     end
 
-    it 'converts a negative integer into English words' do
+    it 'converts a one- and two-digit negative integer into English words' do
       expect(Converter::EnglishNumber.new(-6).in_english).to eq("negative six")
       expect(Converter::EnglishNumber.new(-11).in_english).to eq("negative eleven")
       expect(Converter::EnglishNumber.new(-22).in_english).to eq("negative twenty-two")
       expect(Converter::EnglishNumber.new(-30).in_english).to eq("negative thirty")
     end
 
-    it 'converts a negative decimal into English words' do
+    it 'converts a one- and two-digit negative decimal into English words' do
       expect(Converter::EnglishNumber.new(-6.5).in_english).to eq("negative six")
       expect(Converter::EnglishNumber.new(-24.5).in_english).to eq("negative twenty-four")
     end
 
-    it 'returns nil for numbers that are greater/less than 99/-99' do
-      expect(Converter::EnglishNumber.new(101).in_english).to eq(nil)
-      expect(Converter::EnglishNumber.new(-101).in_english).to eq(nil)
+    # it 'returns nil for numbers that are greater/less than 99/-99' do
+    #   expect(Converter::EnglishNumber.new(101).in_english).to eq(nil)
+    #   expect(Converter::EnglishNumber.new(-101).in_english).to eq(nil)
+    # end
+
+    it 'converts a three-digit positive integer into English words' do
+      expect(Converter::EnglishNumber.new(100).in_english).to eq("one hundred")
+      expect(Converter::EnglishNumber.new(101).in_english).to eq("one hundred one")
+      expect(Converter::EnglishNumber.new(111).in_english).to eq("one hundred eleven")
+      expect(Converter::EnglishNumber.new(123).in_english).to eq("one hundred twenty-three")
+      expect(Converter::EnglishNumber.new(687).in_english).to eq("six hundred eighty-seven")
+      expect(Converter::EnglishNumber.new(687.78).in_english).to eq("six hundred eighty-seven")
     end
 
-    it 'converts 0 to "zero"' do
-      expect(Converter::EnglishNumber.new(0).in_english).to eq("zero")
-      expect(Converter::EnglishNumber.new(0.5).in_english).to eq("zero")
-      expect(Converter::EnglishNumber.new(-0.5).in_english).to eq("zero")
+    it 'converts a three-digit negative integer into English words' do
+      expect(Converter::EnglishNumber.new(-100).in_english).to eq("negative one hundred")
+      expect(Converter::EnglishNumber.new(-101).in_english).to eq("negative one hundred one")
+      expect(Converter::EnglishNumber.new(-111).in_english).to eq("negative one hundred eleven")
+      expect(Converter::EnglishNumber.new(-123).in_english).to eq("negative one hundred twenty-three")
+      expect(Converter::EnglishNumber.new(-687).in_english).to eq("negative six hundred eighty-seven")
+      expect(Converter::EnglishNumber.new(-687.78).in_english).to eq("negative six hundred eighty-seven")
     end
+
+    it 'converts a four- to six-digit positive integer into English words' do
+      expect(Converter::EnglishNumber.new(1000).in_english).to eq("one thousand")
+      expect(Converter::EnglishNumber.new(1001).in_english).to eq("one thousand one")
+      expect(Converter::EnglishNumber.new(1011).in_english).to eq("one thousand eleven")
+      expect(Converter::EnglishNumber.new(1023).in_english).to eq("one thousand twenty-three")
+      expect(Converter::EnglishNumber.new(1687).in_english).to eq("one thousand six hundred eighty-seven")
+      expect(Converter::EnglishNumber.new(1687.78).in_english).to eq("one thousand six hundred eighty-seven")
+    end
+
+    it 'converts a four- to six-digit negative integer into English words' do
+      expect(Converter::EnglishNumber.new(-1000).in_english).to eq("negative one thousand")
+      expect(Converter::EnglishNumber.new(-1001).in_english).to eq("negative one thousand one")
+      expect(Converter::EnglishNumber.new(-1011).in_english).to eq("negative one thousand eleven")
+      expect(Converter::EnglishNumber.new(-1023).in_english).to eq("negative one thousand twenty-three")
+      expect(Converter::EnglishNumber.new(-1687).in_english).to eq("negative one thousand six hundred eighty-seven")
+      expect(Converter::EnglishNumber.new(-1687.78).in_english).to eq("negative one thousand six hundred eighty-seven")
+    end
+
   end
 end
